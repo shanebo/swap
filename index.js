2098cea2a82dd57ba07872d24e01406c69c0b5bc
Shane Thacker <shane@steadymade.com>
2019-05-21 01:22:04 -0500

    latest

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const addEvent = (when, pattern, handle) => {
-  swap.events[when].push(buildRoute(location.origin + pattern, handle));
+const swap = {
+  routes: [],
+  elements: {
+    before: [],
+    on: [],
+    off: []
+  }
+};
+
+swap.listener = function(method, pattern, handle) {
+  if (pattern.startsWith('/')) {
+    if (pattern === '*') pattern = '.*';
+    swap.routes.push(buildRoute(method, location.origin + pattern, handle));
+  } else {
+    swap.elements[method].push({ selector: pattern, handle });
+  }
   return swap;
 }
 
-const swap = {};
-swap.events = { before: [], on: [] };
-swap.on = addEvent.bind(null, 'on');
-swap.before = addEvent.bind(null, 'before');
+// ['before', 'off', 'on'].forEach(method => {
+//   swap[method] = swap.listener.bind(swap, method);
+// });
+
+swap.before = swap.listener.bind(swap, 'before');
+swap.on = swap.listener.bind(swap, 'on');
+swap.off = swap.listener.bind(swap, 'off');
 
 swap.with = (href, selectors = []) => {
   fetch(href)
     .then(res => res.text())
     .then(html => {
+      swap.fire('off', location.href);
       swap.to(html, selectors);
       window.history.pushState({ html, selectors }, '', href);
       if (!selectors.length) window.scrollTo(0, 0);
@@ -23,22 +41,22 @@ swap.with = (href, selectors = []) => {
 
 swap.to = (html, selectors) => {
   const dom = new DOMParser().parseFromString(html, 'text/html');
+  const oldEls = selectors.map(sel => document.querySelector(sel)).filter(el => el);
+  const fullSwap = (selectors.length === 0 || oldEls.length !== selectors.length);
 
-  if (selectors.length) {
-    // accounts for back/forward buttons where selectors no longer exist
-    // in which cases we replace the entire page
-    for (i = 0; i < selectors.length; i++) {
-      const selector = selectors[i];
-      const oldEl = document.querySelector(selector);
-      if (!oldEl) {
-        document.body = dom.body;
-        break;
-      }
-      const newEl = dom.querySelector(selector);
-      oldEl.parentNode.replaceChild(newEl, oldEl);
-    }
-  } else {
+  swap.elements.off.forEach(el => {
+    const target = document.querySelector(el.selector);
+    if (target) el.handle({ target });
+  });
+
+  if (fullSwap) {
     document.body = dom.body;
+  } else {
+    selectors.forEach((sel, s) => {
+      const oldEl = oldEls[s];
+      const newEl = dom.querySelector(sel);
+      oldEl.parentNode.replaceChild(newEl, oldEl);
+    });
   }
 
   document.head = dom.head;
@@ -46,41 +64,65 @@ swap.to = (html, selectors) => {
   return swap;
 }
 
-swap.fire = (when, url) => {
-  const link = document.createElement('a');
-  link.setAttribute('href', url);
-  const pathname = link.origin + link.pathname;
-  const query = parseQuery(link.search);
+swap.fire = (method, url) => {
+  const req = buildRequest(method, url);
 
-  swap.events[when].forEach((route) => {
-    if (route.pattern === pathname) {
-      return route.handle({ params: {}, query });
-    }
-
-    const matches = pathname.match(route.regex);
-
-    if (matches) {
-      matches.shift();
-      const e = { params: {}, query };
-      let i = 0;
-      for (; i < route.params.length; i++) {
-        e.params[route.params[i]] = matches[i];
+  if (method === 'before' || method === 'on') {
+    swap.elements[method].forEach(el => {
+      const target = document.querySelector(el.selector);
+      if (target) {
+        req.target = target;
+        el.handle(req);
       }
-      route.handle(e);
-    }
+    });
+  }
+
+  swap.routes.forEach((route) => {
+    const handle = findRoute(req, route);
+    if (handle) return handle(req);
   });
   return swap;
 }
 
-const buildRoute = (pattern, handle) => {
+swap.event = function(name, delegate, fn) {
+  const e = {
+    name,
+    target: window,
+    fn: arguments.length !== 3
+      ? arguments[1]
+      : delegateHandle(delegate, fn)
+  };
+
+  window.addEventListener(e.name, e.fn);
+  return swap;
+}
+
+
+
+const findRoute = (req, route) => {
+  req.params = {};
+
+  if (route.method !== req.method) return false;
+  if (route.pattern === req.pathname) return route.handle;
+
+  const matches = req.pathname.match(route.regex);
+
+  if (matches) {
+    matches.shift();
+    let i = 0;
+    for (; i < route.params.length; i++) {
+      req.params[route.params[i]] = matches[i];
+    }
+    return route.handle;
+  }
+
+  return false;
+}
+
+const buildRoute = (method, pattern, handle) => {
   const regex = patternRegex(pattern);
   const params = getParams(pattern, regex);
-  return {
-    pattern,
-    regex,
-    params,
-    handle
-  }
+  return { method, pattern, regex, params, handle };
 }
 
 const patternRegex = (pattern) =>
@@ -96,6 +138,16 @@ const getParams = (pattern, regex) => {
   return [];
 }
 
+const buildRequest = (method, url) => {
+  const link = document.createElement('a');
+  link.setAttribute('href', url);
+  return {
+    method,
+    pathname: link.origin + link.pathname,
+    query: parseQuery(link.search)
+  };
+}
+
 const parseQuery = (search) => decodeURIComponent(search).substr(1)
   .split('&')
   .reduce((params, keyval) => {
@@ -104,26 +156,17 @@ const parseQuery = (search) => decodeURIComponent(search).substr(1)
       return params;
     }, {});
 
-const EventDelegator = function(target, name, delegate, fn) {
-  return arguments.length !== 4
-    ? target.addEventListener(name, arguments[2])
-    : target.addEventListener(name, function(e) {
-        if (e.target.matches(delegate)) {
-          return fn.apply(e.target, arguments);
-        }
-      });
-}
-
-// HANDLES
 let metaKeyOn = false;
 
 const loaded = (e) => swap.fire('on', location.href);
 
 const popstate = (e) => {
+  const { href } = location;
   const { html, selectors } = e.state;
+  swap.fire('off', href);
   swap.to(html, selectors);
-  window.history.replaceState({ html, selectors }, '', location.href);
-  swap.fire('on', location.href);
+  window.history.replaceState(e.state, '', href);
+  swap.fire('on', href);
 }
 
 const keyDownUp = (e) => {
@@ -132,16 +175,21 @@ const keyDownUp = (e) => {
   }
 }
 
-const click = (e) => {
-  if (e.target.hostname !== location.hostname
-    || e.target.protocol !== location.protocol) {
+const click = function(e) {
+  const link = this;
+  if (link.hostname !== location.hostname
+    || link.protocol !== location.protocol) {
+    return;
+  }
+
+  if ((link.pathname === location.pathname) && link.hash) {
     return;
   }
 
   if (!metaKeyOn) {
     e.preventDefault();
-    const href = e.target.href;
-    const selectors = (e.target.dataset.swap || '')
+    const href = link.href;
+    const selectors = (link.dataset.swap || '')
       .split(',')
       .filter(selector => selector.trim());
     swap.fire('before', href);
@@ -149,10 +197,164 @@ const click = (e) => {
   }
 }
 
-// SETUP
-window.swap = swap;
-document.addEventListener('DOMContentLoaded', loaded);
+const delegateHandle = function(delegate, fn) {
+  return function(e) {
+    if (e.target.matches(delegate)) {
+      return fn.apply(e.target, arguments);
+    }
+
+    const parent = e.target.closest(delegate);
+
+    if (parent) {
+      return fn.apply(parent, arguments);
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', loaded);
 window.addEventListener('popstate', popstate);
 window.addEventListener('keydown', keyDownUp);
 window.addEventListener('keyup', keyDownUp);
-EventDelegator(document, 'click', 'a:not([target=_blank]):not([data-swap="false"])', click);
+window.addEventListener('click', delegateHandle('a:not([target="_blank"]):not([data-swap="false"])', click));
+
+window.swap = swap;
+
+window.app = swap;
+
+
+
+
+// routes
+// - every route fires when it matches
+// - every route fires off when page state change
+
+// components
+// - has checks run on every page transition
+// - not fires if it was on previous page
+
+
+
+// app.before(route, fn);
+// app.on(route, fn);
+// app.off(route, fn);
+
+// app.before(selector, fn);
+// app.on(selector, fn);
+// app.off(selector, fn);
+
+// app.event(name, selector, fn);
+// // app.off(name, selector, fn);
+
+
+
+// app.on(name, selector, fn);
+// app.off(name, selector, fn);
+// app.emit(name, selector, fn);
+
+// app.before(route, fn);
+// app.on(route, fn);
+// app.off(route, fn);
+// app.emit(route);
+
+// app.before(selector, fn);
+// app.on(selector, fn);
+// app.off(selector, fn);
+
+
+// app.get('/foo', middleware);
+// app.use('/foo', middleware);
+// app.use(handle);
+// app.use('/foo', handle, handle);
+// app.get('/foo', handle, handle);
+// app.use('/foo', handle, handle);
+// app.use('*', handle, handle);
+// app.all('/foo', handle, handle);
+// app.use(handle, handle);
+// app.get('/foo', handle);
+
+
+
+
+
+
+
+// app.click('.ApjNav a', fn);
+
+
+// app.on('click', '.ApjNav a', fn);
+// app.on('.ApjNav', fn);
+
+// app.on('/ask-pastor-john', fn);
+// app.on('.ApjNav', fn);
+
+// app.on('route', '/ask-pastor-john', fn);
+// app.on('route', '.ApjNav', fn);
+
+// app.on(name, selector, fn);
+// app.off(name, selector, fn);
+
+// app.before('route', route, fn);
+// app.on('route', route, fn);
+// app.off('route', route, fn);
+
+// app.before('element', selector, fn);
+// app.on('element', selector, fn);
+// app.off('element', selector, fn);
+
+// app.event('resize', fn);
+// app.event('click', '.btn', fn);
+
+
+// swap.on('*', () => {
+//   let timeout = 0;
+//   swap.event('input', '.ApjSearchInput', (e) => {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       const value = e.target.value;
+//       const url = `${location.origin + location.pathname}?q=${value}`;
+//       swap.with(url, ['.ApjResources']);
+//     }, 10);
+//   });
+// });
+
+
+// app.globalEvent('input', '.ApjSearchInput', (e) => {
+
+// });
+
+
+// swap.has('.ApjSearchInput', () => {
+//   let timeout = 0;
+//   swap.event('input', '.ApjSearchInput', (e) => {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       const value = e.target.value;
+//       const url = `${location.origin + location.pathname}?q=${value}`;
+//       swap.with(url, ['.ApjResources']);
+//     }, 10);
+//   });
+// });
+
+// const scroll = (e) => 'doin something';
+
+// app.on('.btn', (e) => {
+//   btn.addEventListener('scroll', scroll);
+// });
+
+// app.off('.btn', (e) => {
+//   btn.removeEventListener('scroll', scroll);
+// });
+
+// app.on('/foo', (e) => {
+//   btn.addEventListener('scroll', scroll);
+//   window.addEventListener('resize', resize);
+// });
+
+// app.off('/foo', (e) => {
+//   btn.removeEventListener('scroll', scroll);
+// });
+
+// app.on('/foo', (e) => {
+//   btn.addEventListener('scroll', scroll);
+//   app.event('resize', resize);
+// });
