c13a62abb7ec39f1d9a94194cfd471e9b2e5c671
Shane Thacker <shane@steadymade.com>
2020-02-06 04:18:52 -0600

    Delete unused files

--- explored api rejected.js
+++
@@ -1,6 +0,0 @@
-app.on('has:beforeRoute', '.btn', handle);
-app.on('has:onRoute', '.btn', handle);
-app.on('has:offRoute', '.btn', handle);
-app.on('route', '/foo', handle);
-app.on('click', '.btn', handle);
-app.on('atelunch', handle);
