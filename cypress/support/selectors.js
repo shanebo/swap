3122594a1a2152d32f9644be46c78bfb4941311c
Shane Thacker <shane@steadymade.com>
2020-02-25 14:54:13 -0600

    Centralize selectors for use across tests

---
+++ cypress/support/selectors.js
@@ -0,0 +1,7 @@
+module.exports = {
+  qsPane: '.Pane',
+  qsPaneContent: '.Pane.is-active .Pane-content',
+  qsPaneCloseBtn: '.Pane.is-active .Pane-closeBtn',
+  qsPaneIsOpen: '.swap-pane-is-open',
+  qsIsLoading: '.swap-is-loading'
+};
